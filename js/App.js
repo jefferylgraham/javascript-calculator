e6096254239c21e3b9f20f36e7f061e4dda4a26c
Jeffery L. Graham <jeffery.lemar.graham@gmail.com>
2018-09-10 19:24:52 -0400

    add to github pages

---
+++ js/App.js
@@ -0,0 +1,214 @@
+class Buttons extends React.Component {
+  render() {
+    return (
+      <div id="buttons">
+        <button
+          id="decimal"
+          className="numbers"
+          value="."
+          onClick={this.props.numbers}
+        >
+          .
+        </button>
+        <button
+          id="zero"
+          className="numbers"
+          value="0"
+          onClick={this.props.numbers}
+        >
+          0
+        </button>
+        <button
+          id="one"
+          className="numbers"
+          value="1"
+          onClick={this.props.numbers}
+        >
+          1
+        </button>
+        <button
+          id="two"
+          className="numbers"
+          value="2"
+          onClick={this.props.numbers}
+        >
+          2
+        </button>
+        <button
+          id="three"
+          className="numbers"
+          value="3"
+          onClick={this.props.numbers}
+        >
+          3
+        </button>
+        <button
+          id="four"
+          className="numbers"
+          value="4"
+          onClick={this.props.numbers}
+        >
+          4
+        </button>
+        <button
+          id="five"
+          className="numbers"
+          value="5"
+          onClick={this.props.numbers}
+        >
+          5
+        </button>
+        <button
+          id="six"
+          className="numbers"
+          value="6"
+          onClick={this.props.numbers}
+        >
+          6
+        </button>
+        <button
+          id="seven"
+          className="numbers"
+          value="7"
+          onClick={this.props.numbers}
+        >
+          7
+        </button>
+        <button
+          id="eight"
+          className="numbers"
+          value="8"
+          onClick={this.props.numbers}
+        >
+          8
+        </button>
+        <button
+          id="nine"
+          className="numbers"
+          value="9"
+          onClick={this.props.numbers}
+        >
+          9
+        </button>
+        <button id="clear" onClick={this.props.clear}>
+          AC
+        </button>
+        <button
+          id="divide"
+          className="functions"
+          value="/"
+          onClick={this.props.functions}
+        >
+          /
+        </button>
+        <button
+          id="multiply"
+          className="functions"
+          value="*"
+          onClick={this.props.functions}
+        >
+          *
+        </button>
+        <button
+          id="add"
+          className="functions"
+          value="+"
+          onClick={this.props.functions}
+        >
+          +
+        </button>
+        <button
+          id="subtract"
+          className="functions"
+          value="-"
+          onClick={this.props.functions}
+        >
+          -
+        </button>
+        <button
+          id="equals"
+          className="functions"
+          value="="
+          onClick={this.props.equals}
+        >
+          =
+        </button>
+      </div>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      output: "",
+      display: 0
+    };
+    this.handleNumbers = this.handleNumbers.bind(this);
+    this.handleFunctions = this.handleFunctions.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+    this.handleEquals = this.handleEquals.bind(this);
+  }
+
+  handleClear() {
+    this.setState({
+      output: "",
+      display: 0
+    });
+  }
+
+  handleNumbers(e) {
+    if (e.target.value == 0 && this.state.output == "") {
+      console.log("zero cant start.");
+    } else if (e.target.value == "." && this.state.output == "") {
+      this.setState({
+        output: "0" + e.target.value,
+        display: e.target.value
+      });
+    } else if (e.target.value == "." && this.state.output.indexOf(".") > 0) {
+      console.log("Already have a decimal");
+    } else {
+      this.setState({
+        output: (this.state.output += e.target.value),
+        display: e.target.value
+      });
+    }
+  }
+
+  handleFunctions(e) {
+    this.setState({
+      output: (this.state.output += e.target.value),
+      display: e.target.value
+    });
+  }
+
+  handleEquals(e) {
+    this.setState({
+      output: (this.state.output += e.target.value)
+    });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <div id="calculator">
+            <div id="upper">
+              <div id="output">{this.state.output}</div>
+              <div id="display">{this.state.display}</div>
+            </div>
+            <Buttons
+              numbers={this.handleNumbers}
+              functions={this.handleFunctions}
+              clear={this.handleClear}
+              equals={this.handleEquals}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Calculator />, document.getElementById("root"));
